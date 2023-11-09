9ae693ebbb610cfdf044dcb890ad29e79753e5cb
Abolfazltabatabaee <149058638+Abolfazltabatabaee@users.noreply.github.com>
2023-11-09 14:25:02 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//AbuTabatabaee
+
+
+int n = 1;
+int m;
+int q = 0;
+Console.Write("enter a NO.: ");
+m = int.Parse(Console.ReadLine());
+for (int x = 1; x <= m; x++)
+
+{
+    q = (x * (x + 1)) / 2;
+
+    Console.WriteLine(q);
+}
